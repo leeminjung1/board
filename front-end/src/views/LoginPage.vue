76442f06fccbf1b67e4b2aca7d2de638e5434afa
leeminjung1 <vipminjung@gmail.com>
2022-07-23 14:22:07 +0900

    first commit

---
+++ front-end/src/views/LoginPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>TaskAgile</h1>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
---
+++ front-end/tests/unit/LoginPage.spec.js
@@ -0,0 +1,13 @@
+//import { defineComponent } from '@vue/composition-api'
+import Vue from 'vue'
+import LoginPage from '@/views/LoginPage'
+
+describe('LoginPage.vue', () => {
+  it('should render correct contents', () => {
+
+    const Constructor = Vue.extend(LoginPage)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('h1').textContent)
+      .toEqual('TaskAgile')
+  })
+})
